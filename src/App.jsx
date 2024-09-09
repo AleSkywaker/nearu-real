9aa276ec20c923dc66ae8c986c76825bbc35f17b
Alessandro <alexsandrocolombo@gmail.com>
2024-09-09 19:30:31 +0200

    added some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './layout.scss'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Alex</h1>
+      <div className='layout'>
+        Alex
+      </div>
     </>
   )
 }
